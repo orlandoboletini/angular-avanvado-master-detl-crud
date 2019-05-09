4d5f58416057c41bec3613ec717529cc2a9b6c9f
Orlando Scotto Boletini <Orlando@vulcabras.corp>
2019-05-09 13:37:21 -0300

    Botao excluir

--- src/app/pages/categories/shared/category.service.ts
+++ src/app/pages/categories/shared/category.service.ts
@@ -24,7 +24,7 @@ export class CategoryService {
   }
 
   getByID(id: number ):Observable<Category>{
-    const url = '${this.apiPath}/${id}';
+    const url = `${this.apiPath}/${id}`;
     return this.http.get(url).pipe(
       catchError(this.handleError),
       map(this.jsonDataToCategory)
@@ -39,7 +39,7 @@ export class CategoryService {
   }
 
   update(category: Category): Observable<Category> {
-    const url = '${this.apiPath}/${category.id}';
+    const url = `${this.apiPath}/${category.id}`;
     return this.http.put(url, category).pipe(
       catchError(this.handleError),
       map(() => category)
@@ -47,7 +47,7 @@ export class CategoryService {
   }
 
   delete(id: number): Observable<any> {
-    const url = '${this.apiPath}/${id}';
+    const url = `${this.apiPath}/${id}`;
     return this.http.delete(url).pipe(
       catchError(this.handleError),
       map(() => null)
