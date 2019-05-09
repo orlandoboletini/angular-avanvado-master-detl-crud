e70bbb196c74980604616d3727f6728ac923b7aa
Orlando Scotto Boletini <Orlando@vulcabras.corp>
2019-05-09 11:02:54 -0300

    Add Service Category

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,9 +1,10 @@
 import {InMemoryDbService} from "angular-in-memory-web-api";
+import {Category} from "./pages/categories/shared/category-model";
 
 export class InMemoryDatabase implements InMemoryDbService {
 
     createDb(){
-        const categories =  [
+        const categories:Category[] =  [
             {id: 1,name: "Lazer", description: "Ciname, Park,etc"},
             {id: 1,name: "Moradia", description: "Pagamento de Conta da Casa"},
             {id: 1,name: "Saúde", description: "Pagamento do Plano se Saúde"},
