6defae01cf4a065aef6d1bd7665a95e1ce5b621c
Orlando Scotto Boletini <Orlando@vulcabras.corp>
2019-05-09 08:38:58 -0300

    Add congig modules

--- src/app/pages/categories/categories-routing.module.ts
+++ src/app/pages/categories/categories-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoriyListComponent } from './categoriy-list/categoriy-list.component';
+import { CategoriyFormComponent } from './categoriy-form/categoriy-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: CategoriyListComponent},
+  {path: 'new', component: CategoriyFormComponent},
+  {path: ':id/edit', component: CategoriyFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
